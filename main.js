d42a6e10ae59723d417e95fe12560c7386750e16
CarlosAlbertoVillafradesCAMPUS <cavillafrades@gmail.com>
2023-03-09 08:47:57 -0500

    feat(Operadores-Math): :rocket: Math.round
    Explicacion con ejemplo del operador Math.round
    
    by:Carlos Villafrades

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
-//Math.floor()()
+//Math.round()
 
 let num1 = 3.3567;
-console.log(`Math.floor()`);
-console.log(`\t Math.floor()(${num1}) = ${Math.floor(num1)}`);
+let num2 = 3.6195;
+console.log(`Math.round()`);
+console.log(`\t Math.round(${num1}) = ${Math.round(num1)}`);
+console.log(`\t Math.round(${num2}) = ${Math.round(num2)}`);
 
