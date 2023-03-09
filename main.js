96e533b14b0189e02685757a22080220e108fa23
CarlosAlbertoVillafradesCAMPUS <cavillafrades@gmail.com>
2023-03-08 21:19:17 -0500

    feat:[Operadores-Logicos]🚀 descripcion operadores logicos
    explicacion con ejemplos decada uno de los operadores logicos que existen en javascript
    
    by:Carlos Villafrades

--- main.js
+++ main.js
@@ -1,37 +1,17 @@
 let numero1 = 10;
 let numero2 = 5
 
-//Igual (==)
-console.log(`Igual (==)`)
-console.log(`\t${numero1} == "${numero1}" ${numero1==numero1}`)
+//AND Lógico (&&)
+console.log(`AND Lógico (&&)`)
+console.log(`\t((${numero1} == "${numero1}") && (${numero2} == "${numero2}") ) ${numero1==numero1 && numero2 == numero2}`)
 
-//No es igual (!=)
-console.log(`No es igual (!=)`)
-console.log(`\t${numero1} != "${numero2}" ${numero1!=numero2}`)
-
-//Estrictamente igual (===)
-console.log(`Estrictamente igual (===)`)
-console.log(`\t${numero1} === ${numero1} ${numero1===numero1}`)
-console.log(`\t${numero1} === "${numero1}" ${numero1===numero2}`)
-
-//Desigualdad estricta (!==)
-console.log(`Desigualdad estricta (!==)`)
-console.log(`\t${numero1} !== "${numero1}" ${numero1!==numero2}`)
-console.log(`\t${numero1} !== ${numero1} ${numero1!==numero1}`)
+//OR lógico (||)
+console.log(`OR lógico (||)`)
+console.log(`\t((${numero1} == ${numero2}) || (${numero1} > ${numero2}) ) ${numero1 == numero2 || numero1 > numero2}`)
 
 
-//Mayor que (>)
-console.log(`Mayor que (>)`)
-console.log(`\t${numero1} > ${numero2} ${numero1 > numero2}`)
+//NOT lógico (!)
+console.log(`NOT lógico (!)`)
+console.log(`\t!true = ${!true}`)
+console.log(`\t!false = ${!false}`)
 
-//Mayor o igual que (>=)
-console.log(`Mayor o igual que (>=)`)
-console.log(`\t${numero1} >= ${numero1} ${numero1 >= numero1}`)
-
-//Menor que (<)
-console.log(`Menor que (<)`)
-console.log(`\t${numero2} < ${numero1} ${numero2 < numero1}`)
-
-//Menor o igual (<=)
-console.log(`Menor o igual (<=)`)
-console.log(`\t${numero1} <= ${numero1} ${numero1 <= numero1}`)
