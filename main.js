5ed8541965b96d069492c92ff52cad86933a83d7
CarlosAlbertoVillafradesCAMPUS <cavillafrades@gmail.com>
2023-03-09 08:44:42 -0500

    feat(Operadores-Math): :rocket: Math.floor
    Explicacion con ejemplo del operador Math.floor
    
    by:Carlos Villafrades

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-//Math.ceil()
+//Math.floor()()
 
 let num1 = 3.3567;
-console.log(`Math.ceil()`);
-console.log(`\t Math.ceil(${num1}) = ${Math.ceil(num1)}`);
+console.log(`Math.floor()`);
+console.log(`\t Math.floor()(${num1}) = ${Math.floor(num1)}`);
 
