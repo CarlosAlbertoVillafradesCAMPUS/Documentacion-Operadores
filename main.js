6e978d11dea16c7bbdebcffbc7d77baacb245ead
jjMorgar <jjmorgar.co@gmail.com>
2023-03-09 08:13:38 -0500

    feat(Operadores-Math): :rocket: Math.PI
    Explicacion con ejemplo del operador Math.PI
    
    by:Carlos Villafrades

--- main.js
+++ main.js
@@ -1,17 +1,5 @@
-let numero1 = 10;
-let numero2 = 5
+//Math.PI
 
-//AND Lógico (&&)
-console.log(`AND Lógico (&&)`)
-console.log(`\t((${numero1} == "${numero1}") && (${numero2} == "${numero2}") ) ${numero1==numero1 && numero2 == numero2}`)
-
-//OR lógico (||)
-console.log(`OR lógico (||)`)
-console.log(`\t((${numero1} == ${numero2}) || (${numero1} > ${numero2}) ) ${numero1 == numero2 || numero1 > numero2}`)
-
-
-//NOT lógico (!)
-console.log(`NOT lógico (!)`)
-console.log(`\t!true = ${!true}`)
-console.log(`\t!false = ${!false}`)
+console.log(`Math.PI`);
+console.log(`\tMath.PI = ${Math.PI}`);
 
