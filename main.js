d7bc736aa157b1eeb153e415c2a03b230644fad4
CarlosAlbertoVillafradesCAMPUS <cavillafrades@gmail.com>
2023-03-08 20:47:48 -0500

    feat:[Operadores-Aritmeticos]🚀 descripcion operadores aritmeticos
    explicacion con ejemplos decada uno de los operadores aritmeticos que existen en javascript
    
    by:Carlos Villafrades

---
+++ main.js
@@ -0,0 +1,26 @@
+let numero1 = 10;
+let numero2 = 5
+
+//Residuo (%)
+console.log(`Residuo (%)`)
+console.log(`\t${numero1} % ${numero2} = ${numero1 % numero2}`)
+
+//Incremento (++)
+console.log(`Incremento (++)`)
+console.log(`\t++${numero1} = ${++numero1}`)
+
+//Decremento (--)
+console.log(`Decremento (--)`)
+console.log(`\t--${numero1} = ${--numero1}`)
+
+//Negación unaria (-)
+console.log(`Negación unaria (-)`)
+console.log(`\t${numero1}\n-${numero1} = ${-numero1}`)
+
+//Positivo unario (+)
+console.log(`Positivo unario (+)`)
+console.log(`\t+"${numero1}" = ${+numero1}`)
+
+//Operador de exponenciación (**)
+console.log(`Operador de exponenciación (**)`)
+console.log(`\t${numero2} ** ${numero1} = ${numero2 ** numero1}`)
